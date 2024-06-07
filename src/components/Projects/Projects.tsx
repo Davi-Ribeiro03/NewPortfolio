9a0405273a36967fb2530b74e7128e344ba9f291
Davi Ribeiro <davi.75.ef@gmail.com>
2024-06-07 09:49:54 -0300

    feat: adiciona texto nos projetos e adiciona paginação nos projetos em layout mobile

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,82 +7,111 @@ import idak from "../../assets/idak.png";
 import react from "../../assets/react.png";
 import html from "../../assets/html.png";
 import css from "../../assets/css.png";
-
-// import next from "../../assets/next.png";
 import typescript from "../../assets/typescript.png";
+import redux from "../../assets/redux.svg";
+import next from "../../assets/next.png";
+import figma from "../../assets/figma.png";
+import tailwind from "../../assets/tailwind.png";
 
 import "./Projects.css";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
-const projetos = [
+interface ProjetosType {
+  id: number;
+  img: string;
+  url: string;
+  name: string;
+  description: string;
+  technologies: string[];
+}
+
+const projetos: ProjetosType[] = [
   {
+    id: 0,
     img: eletronic,
     url: "https://eletronic.vercel.app/",
     name: "Eletronic",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex recusandae libero ullam obcaecati facilis praesentium quod non minus.",
+      "Projeto que desenvolvi para estudar alguns conceitos, como redux com middleware thunk, context, rotas etc. Nesse projeto há uma página de login, carrinho de compras e um dashboard simples para admin.",
+    technologies: [html, css, react, typescript, redux],
   },
   {
+    id: 1,
     img: gym,
     url: "https://gym-it2y.vercel.app/",
     name: "Gym",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex recusandae libero ullam obcaecati facilis praesentium quod non minus.",
+      "Landing page simples criada em React. Encontrei esse desgin e decidi recriá-lo, como não tinha o figma, mudei algumas coisas",
+    technologies: [html, css, react, typescript],
   },
   {
+    id: 2,
     img: pokedex,
     url: "https://pokedex-react-git-develop-felipebarreto-osfdigital.vercel.app/",
     name: "Pokedex",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex recusandae libero ullam obcaecati facilis praesentium quod non minus.",
+      "Primeiro projeto que criei em react e typescript, utilizei uma api já pronta e criei o design do zero. Nesse projeto aprendi técnicas como paginação, rotas, consumo de api com axios etc.",
+    technologies: [html, css, react, typescript, redux],
   },
   {
+    id: 3,
     img: quiz,
     url: "https://quiz-daviribeiro03s-projects.vercel.app/",
     name: "Quiz",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex recusandae libero ullam obcaecati facilis praesentium quod non minus.",
+      "Projeto criado para praticar um pouco sobre next. Nesse projeto criei uma api utilizando Json server e consumo através do axios, além disso utilizei técnias integradas no next como SSR para melhorar o desempenho da aplicação",
+    technologies: [html, css, next, typescript, figma, tailwind],
   },
   {
+    id: 4,
     img: idak,
-    url: "",
+    url: "https://idak-silk.vercel.app/",
     name: "Idak",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex recusandae libero ullam obcaecati facilis praesentium quod non minus.",
-  },
-  {
-    img: producao,
-    url: "",
-    name: "Project",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex recusandae libero ullam obcaecati facilis praesentium quod non minus.",
+      "Landing page criada para praticar um pouco sobre next e tailwindcss",
+    technologies: [html, css, next, typescript, tailwind],
   },
 ];
 
 const Projects = () => {
+  const [projetosMobile, setProjetosMobile] = useState<ProjetosType[]>([]);
+
+  useEffect(() => {
+    if (window.innerWidth < 820) {
+      setProjetosMobile(projetos.filter((projeto) => projeto.id < 2));
+    } else {
+      setProjetosMobile(projetos);
+    }
+  }, []);
+
+  function acrescentaProjectsMobile() {
+    setProjetosMobile(
+      projetos.filter((projeto) => projeto.id < projetosMobile?.length + 2)
+    );
+  }
+
   return (
     <div className="projects" id="projects">
       <h2>Projects</h2>
 
       <div className="containerProjectsImg">
-        {projetos.map((projeto, index) => (
+        {projetosMobile?.map((projeto, index) => (
           <motion.div
             className="cardProjectImg"
             key={index}
             initial={{ opacity: 0, x: -80 }}
             whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: index * 0.5 }}
-            viewport={{ once: true }}
+            transition={{ duration: 0.5 }}
           >
             <img src={projeto.img} alt="" className="projectImage" />
             <div className="projectDescription">
               <p className="name">{projeto.name}</p>
               <p className="description">{projeto.description}</p>
               <section className="technologies">
-                <img src={react} alt="" />
-                <img src={html} alt="" />
-                <img src={css} alt="" />
-                <img src={typescript} alt="" />
+                {projeto.technologies.map((tech, index) => (
+                  <img src={tech} alt="" key={index} />
+                ))}
               </section>
               <a href={projeto.url} target="_blank" rel="noopener noreferrer">
                 <button>See more</button>
@@ -91,6 +120,16 @@ const Projects = () => {
           </motion.div>
         ))}
       </div>
+      <button
+        onClick={acrescentaProjectsMobile}
+        className={
+          projetos.length > projetosMobile?.length
+            ? "buttonVisible"
+            : "buttonHidden"
+        }
+      >
+        See more Projects
+      </button>
     </div>
   );
 };
