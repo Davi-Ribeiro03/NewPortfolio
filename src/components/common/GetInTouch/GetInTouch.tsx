0895c8d711a0cdb0a59a6bae0d171ab069d3134c
Davi Ribeiro <davi.75.ef@gmail.com>
2025-12-18 17:58:15 -0300

    add filled option in get in touch button

--- src/components/common/GetInTouch/GetInTouch.tsx
+++ src/components/common/GetInTouch/GetInTouch.tsx
@@ -6,15 +6,16 @@ type ButtonPaddings = "5px 12px" | "10px 32px";
 interface ButtonProps {
   title: string;
   padding?: ButtonPaddings;
+  filled?: boolean;
 }
 
-const GetInTouch = ({ title, padding }: ButtonProps) => {
+const GetInTouch = ({ title, padding, filled }: ButtonProps) => {
   return (
     <a
       href="https://wa.me/5585981721495"
       target="_blank"
       rel="noopener noreferrer"
-      className="getInTouchButton"
+      className={`getInTouchButton ${filled && "filled"}`}
       style={{ padding: padding }}
     >
       {title}
