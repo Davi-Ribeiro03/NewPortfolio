16f164b22e7896c569cd0bf015c3b572f827ddcb
Davi Ribeiro <davi.75.ef@gmail.com>
2025-12-19 17:24:11 -0300

    fix responsiveness

--- src/components/Thanks/Thanks.tsx
+++ src/components/Thanks/Thanks.tsx
@@ -1,9 +1,11 @@
+import CardRedesSocias from "../common/CardRedesSociais/CardRedesSocias";
 import "./Thanks.css";
 
 const Thanks = () => {
   return (
     <div className="thanks">
       <h2>"Thanks for Scrolling"</h2>
+      <CardRedesSocias />
     </div>
   );
 };
